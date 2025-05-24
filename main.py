6f0aa13820e5fd4053bf3f6e788c383b2bcd3242
bgembly <gilbriem@gmail.com>
2025-05-23 21:09:10 -0400

    Final

--- main.py
+++ main.py
@@ -2,12 +2,32 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable,drawable, shots)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    af = AsteroidField()
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
@@ -15,8 +35,30 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+            
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print("Game over!")
+                sys.exit("Game Over!")
+
+            for shot in shots:
+                if asteroid.collision_check(shot):
+                    asteroid.split()
+                    shot.kill()
+
+        
+
+        screen.fill("black")
+
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+        print(dt)
 
 
  #   print("Starting Asteroids!")
